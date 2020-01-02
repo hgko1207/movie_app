64459604301cab82226ad93a35d78d14e59f7d31
hgko <hgko@inspace.re.kr>
2020-01-02 15:26:20 +0900

    All you need to know about state

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <img src={picture} />
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    count: 0
+  };
 
-const foodLike = [
-  {
-    name: "Kimchi",
-    image: "https://assets.epicurious.com/photos/57990a15437fcffe02f722fd/6:4/w_620%2Ch_413/traditional-napa-cabbage-kimchi.jpg"
-  },
-  {
-    name: "Samgyeopsal",
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-pNQI_9YAcJM%2FVBTxavO6YMI%2FAAAAAAAACk4%2F-OHN-keA9UU%2Fs1600%2F20140909_204156.jpg&f=1&nofb=1"
-  },
-];
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  };
 
-function App() {
-  return (
-    <div>
-      {foodLike.map(dish =>
-        <Food name={dish.name} picture={dish.image} />
-      )}
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
