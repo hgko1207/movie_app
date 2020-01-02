02411c77931fa64667a4148c423a33fe6d9df749
hgko <hgko@inspace.re.kr>
2020-01-02 14:19:58 +0900

    use map in javascript

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <img src={picture} />
+    </div>
+  );
+}
+
+const foodLike = [
+  {
+    name: "Kimchi",
+    image: "https://assets.epicurious.com/photos/57990a15437fcffe02f722fd/6:4/w_620%2Ch_413/traditional-napa-cabbage-kimchi.jpg"
+  },
+  {
+    name: "Samgyeopsal",
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-pNQI_9YAcJM%2FVBTxavO6YMI%2FAAAAAAAACk4%2F-OHN-keA9UU%2Fs1600%2F20140909_204156.jpg&f=1&nofb=1"
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodLike.map(dish =>
+        <Food name={dish.name} picture={dish.image} />
+      )}
     </div>
   );
 }
